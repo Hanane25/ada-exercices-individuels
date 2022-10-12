c45c96190d544e664a95b7b185b45522c5733084
Hanane Essalehi <hanan.essalehi@gmail.com>
2022-10-12 16:41:54 +0200

    add changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 
-var name = prompt("Quelle est votre nom");
+function askName() {
 
-document.querySelector('h2').innerHTML = "👋 Bonjour " + name;
+    var name = prompt("Quelle est votre nom ?");
+    var bonjour = "👋 Bonjour ";
 
-var year = prompt("Quelle est votre année de naissance");
+    document.querySelector('h2').innerHTML = bonjour + name;
 
-var age = 2022 - year;
+}
 
-console.log(age)
+
+function askBirthYear() {
+
+    var year = prompt("Quelle est votre année de naissance ?");
+    var month = prompt("Quelle est votre mois de naissance ?");
+
+    /*function calculAge() {
+        var age = 2022 - year;
+        return age;
+    }*/
+
+    var age = (2022 - year);
+    var birthMonth = (10 - month)
+
+    document.querySelector('h3').innerHTML += age + ' ans et ' + birthMonth + ' mois';
+
+    console.log(age);
+
+}
+
+askName();
+askBirthYear();
+
+
